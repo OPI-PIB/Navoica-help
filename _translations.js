eb4d1cacc4f98c46dcbce28ddbd064360da52053
Aleksander Majda <nazgut@gmail.com>
2020-01-09 14:32:22 +0100

    add new help text #MOOC-793

--- _translations.js
+++ _translations.js
@@ -1,11 +1,11 @@
 
 function hnd_ut(a){
-a.TRANSLATIONS['Search term too short'] = "Search term too short";
-a.TRANSLATIONS['No results'] = "No results";
-a.TRANSLATIONS['Please enter 3 or more characters'] = "Please enter 3 or more characters";
+a.TRANSLATIONS['Search term too short'] = "Wyszukiwana fraza jest zbyt krótka";
+a.TRANSLATIONS['No results'] = "Brak wyników";
+a.TRANSLATIONS['Please enter 3 or more characters'] = "Proszę wpisać co najmniej 3 znaki";
 a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "Word list not ready yet. Please wait until the word list is fully downloaded";
 a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Incorrect or corrupt search data. Please check your HelpNDoc template";
-a.TRANSLATIONS['Related topics...'] = "Related topics...";
-a.TRANSLATIONS['Loading...'] = "Loading...";
-a.TRANSLATIONS['Close'] = "Close";
+a.TRANSLATIONS['Related topics...'] = "Powiązane tematy...";
+a.TRANSLATIONS['Loading...'] = "Wczytywanie...";
+a.TRANSLATIONS['Close'] = "Zamknij";
 }
